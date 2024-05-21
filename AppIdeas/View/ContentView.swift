042f64a5bd0d77f56d165d16450a4bd2efe2f07c
dezdoesit <dez.blair06@gmail.com>
2024-05-21 19:45:46 -0400

    Added supplemental view to add new app ideas, also append to SwiftData model using modelContext.insert

--- AppIdeas/View/ContentView.swift
+++ AppIdeas/View/ContentView.swift
@@ -12,6 +12,12 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query var ideas: [AppIdea]
 
+    @State private var showAddIdea = false
+    @State private var newIdeaName = ""
+    @State private var newIdeaDescription = ""
+
+
+
     var body: some View {
         NavigationStack{
             List(ideas) { idea in
@@ -27,11 +33,34 @@ struct ContentView: View {
             }
             .navigationTitle("App Ideas")
             .toolbar {
-                
+                Button("Add") {
+                    showAddIdea.toggle()
+                }
+            }
+            .sheet(isPresented: $showAddIdea) {
+                NavigationStack {
+                    Form{
+                        TextField("Name", text: $newIdeaName)
+                        TextField("Description", text: $newIdeaDescription)
+
+                    }
+                    .navigationTitle("New App Idea")
+                    .toolbar {
+                        Button("Cancel") {
+                            showAddIdea.toggle()
+                        }
+                        Button("Save") {
+let idea = AppIdea(name: newIdeaName, detailedDescription: newIdeaDescription)
+                            modelContext.insert(idea)
+                            showAddIdea.toggle()
+                        }
+                    }
+                }
+                .presentationDetents([.medium])
             }
         }
-    }
 
+    }
 }
 
 #Preview {
